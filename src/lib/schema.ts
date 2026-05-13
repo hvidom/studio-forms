88572dab82393e8101037c0471b6ea1b13e9b70a
Hryhorii Victorovich Ilin <devopsick@gmail.com>
2026-05-13 15:51:51 +0100

    delete some ts

--- src/lib/schema.ts
+++
@@ -1,8 +0,0 @@
-// src/db/schema.ts
-import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
-
-export const users = sqliteTable('users', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  name: text('name').notNull(),
-  email: text('email').notNull().unique(),
-});
